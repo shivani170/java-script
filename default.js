efefbe5a5905812ca531f2c821effbc1fe8e276a
shivani170 <shivani@devtron.ai>
2021-05-26 13:46:01 +0530

    java test cases

---
+++ default.js
@@ -0,0 +1,4 @@
+alert("I’m JavaScript!");
+alert("I am cool")
+[1, 2].forEach(alert)
+document.write("<b>Hello world!</>")  //document is a object representiong  entire document; write is a method for writing;
